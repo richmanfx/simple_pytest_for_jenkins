35c4b17c82eeaf362d852ef11d473444f166c7df
Aleksandr Jashhuk <richmanfx@yandex.ru>
2017-11-04 22:13:11 +0300

    В провальный тест

--- test_simple_pytest_for_jenkins.py
+++ test_simple_pytest_for_jenkins.py
@@ -11,7 +11,8 @@ class TestNumber1:
     def test_first_step_upper(self):
         assert u'строка'.upper() == u'СТРОКА'
 
-#    @pytest.mark.run(order=2)
-#    @allure.step('Второй шаг - провальная проверка заглавных букв')
-#    def test_second_step_failed_upper(self):
-#        assert u'строка'.upper() == u'СтРоКа'
+    @pytest.mark.run(order=2)
+    @allure.step('Второй шаг - провальная проверка заглавных букв')
+    def test_second_step_failed_upper(self):
+        assert u'строка'.upper() == u'СтРоКа'
+
